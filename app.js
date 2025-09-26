df68fd25444e1394aa76817125f0d945e8a22903
rohan2399 <68842853+rohan2399@users.noreply.github.com>
2025-09-27 00:35:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const products = [
         qikinkProductId: 62944074,
         qikinkVariations: {
             "White": {
-                        "S": "MVnHs-Wh-S",    // Product SKU instead of Variant SKU
+                        "S": "MRnSl-Cm-S",    // Product SKU instead of Variant SKU
                         "M": "MStRnHs-Wh-M", 
                         "L": "MStRnHs-Wh-L",
                         "XL": "MStRnHs-Wh-XL",
